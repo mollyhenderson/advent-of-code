f0ea237e9a4e4c3ffe529e668b6178b1d390be4a
Molly Henderson <henderson.molly4@gmail.com>
2020-11-30 21:42:31 -0500

    2019 day 1, part 2!

--- 2019/day_1/code.js
+++ 2019/day_1/code.js
@@ -1,8 +1,27 @@
 const fs = require('fs');
 
+function calcFuel(n) {
+  return Math.max(0, Math.floor(parseInt(n) / 3) - 2);
+}
+
+function calcFuelRecursive(n) {
+  let sum = 0;
+  while(n > 0) {
+    n = calcFuel(n);
+    sum += n;
+  }
+  return sum;
+}
+
 function answer1(input) {
   return input.reduce((acc, n) => {
-    return acc + Math.floor(parseInt(n) / 3) - 2;
+    return acc + calcFuel(n);
+  }, 0);
+}
+
+function answer2(input) {
+  return input.reduce((acc, n) => {
+    return acc + calcFuelRecursive(n);
   }, 0);
 }
 
@@ -11,7 +30,6 @@ function readInput(filename) {
   return input.split('\r\n');
 }
 
-const input = readInput('2019/day_1/input.txt');
+const input = readInput('input.txt');
 
-console.log(answer1(input));
-
+console.log(answer2(input));
