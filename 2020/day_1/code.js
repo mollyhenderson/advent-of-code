e8bbf8f5ee2676fae8a298dae5ebe73a55902504
Molly Henderson <henderson.molly4@gmail.com>
2020-12-01 19:44:39 -0500

    Day 1!

---
+++ 2020/day_1/code.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function answer1(input) {
+  for (let i of input) {
+    const theOtherOne = input.find(j => i+j === 2020);
+    if (theOtherOne) {
+      return i * theOtherOne;
+    }
+  }
+}
+
+function answer2(input) {
+  for (let i of input) {
+    for (let j of input) {
+      for (let k of input) {
+        if (i + j + k === 2020) {
+          return i * j * k;
+        }
+      }
+    }
+  }
+}
+
+function readInput(filename) {
+  const input = fs.readFileSync(filename, { encoding: 'utf8' });
+  return input.split('\n');
+}
+
+function readInputAsInts(filename) {
+  return readInput(filename).map(i => parseInt(i));
+}
+
+const input = readInputAsInts('input.txt');
+
+console.log(answer2(input));
