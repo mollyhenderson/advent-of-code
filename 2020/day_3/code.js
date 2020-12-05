49e8ce8658e5c862958b52a27ee2dd79b8203915
Molly Henderson <henderson.molly4@gmail.com>
2020-12-04 21:36:42 -0500

    Forgot to commit part 2 for day 3! :scream:

--- 2020/day_3/code.js
+++ 2020/day_3/code.js
@@ -1,9 +1,10 @@
 const { readInput } = require('../../utils');
 
-function answer1(inputs) {
-  let i = -3;
-  return inputs.reduce((sum, row) => {
-    i += 3;
+function answer1(inputs, down, right) {
+  const arr = inputs.filter((_, index) => index % down === 0);
+  let i = -1 * right;
+  return arr.reduce((sum, row) => {
+    i += right;
     if (i > row.length - 1) {
       i = i - row.length;
     }
@@ -12,7 +13,19 @@ function answer1(inputs) {
 }
 
 function answer2(inputs) {
-  
+  let prod = 1;
+  const cases = [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2]
+  ];
+  for (let [right, down] of cases) {
+    const out = answer1(inputs, down, right);
+    prod *= out;
+  }
+  return prod;
 }
 
-console.log(answer1(readInput('input.txt')));
+console.log(answer2(readInput('input.txt')));
