a1a65f6d4d84302c37faf481df64912fdb57f115
Molly Henderson <henderson.molly4@gmail.com>
2020-12-15 00:30:55 -0500

    Part 2!

--- 2020/day_7/code.py
+++ 2020/day_7/code.py
@@ -6,7 +6,7 @@ def answer1(input, matchColor):
   containedIn = defaultdict(list)
   lines = input.split('\n')
   for line in lines:
-    [first, *rest] = re.findall(r'([a-z]*? [a-z]*?) bag', line)
+    [first, *rest] = re.findall(r'([a-z]* [a-z]*) bag', line)
 
     for color in rest:
       containedIn[color].append(first)
@@ -21,6 +21,27 @@ def answer1(input, matchColor):
   
   return len(E) - 1
 
+def answer2(input, matchColor):
+  contains = defaultdict(list)
+  lines = input.split('\n')
+  for line in lines:
+    [first, *rest] = re.findall(r'(\d* ?[a-z]* [a-z]*) bag', line)
+
+    for r in rest:
+      if r == ' no other': continue
+      [n, *color] = r.split(' ')
+      contains[first].extend(' '.join(color) for i in range(int(n)))
+  
+  count = 0
+  V = [ matchColor ]
+  E = []
+  while len(V) > 0:
+    c = V.pop()
+    E.append(c)
+    V.extend(i for i in contains[c])
+  
+  return len(E) - 1
+
 
 f = open('input.txt', 'r')
-print(answer1(f.read(), 'shiny gold'))
+print(answer2(f.read(), 'shiny gold'))
