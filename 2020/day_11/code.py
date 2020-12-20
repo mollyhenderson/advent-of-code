70b722145e63bf85308db82539ef25d1e9612323
Molly Henderson <henderson.molly4@gmail.com>
2020-12-20 18:17:29 -0500

    Day 11, part 2

--- 2020/day_11/code.py
+++ 2020/day_11/code.py
@@ -1,41 +1,64 @@
-from math import factorial
-
 FLOOR = '.'
 EMPTY = 'L'
 OCCUPIED = '#'
 
-def applyRules(map):
+def getAdj1(map, i, j, char):
+  startI = max(0, i-1)
+  startJ = max(0, j-1)
+  adj = ''.join([row[startJ:j+2] for row in map[startI:i+2]])
+  return adj.replace(char, '', 1)
+
+def getAdj2(map, i, j, char):
+  adj = []
+  for upDown in range(-1, 2):
+    for leftRight in range(-1, 2):
+      if (upDown == 0 and leftRight == 0):
+        continue
+      startI = i+upDown
+      startJ = j+leftRight
+      while startI >= 0 and startJ >= 0 and len(map) > startI and len(map[startI]) > startJ:
+        seen = map[startI][startJ]
+        if seen != FLOOR:
+          adj.append(seen)
+          break
+        startI = startI+upDown
+        startJ = startJ+leftRight
+  return adj
+
+
+def applyRules(map, getAdjFn, maxOccupied):
   newMap = []
   for i, row in enumerate(map):
     newMap.append('')
     for j, char in enumerate(row):
-      startI = max(0, i-1)
-      startJ = max(0, j-1)
-      adj = ''.join([row[startJ:j+2] for row in map[startI:i+2]])
-      adj = adj.replace(char, '', 1)
-
+      if char == FLOOR:
+        newMap[i] += char
+        continue
+      adj = getAdjFn(map, i, j, char)
       numOccupied = len([x for x in adj if x == OCCUPIED])
+
       if char == EMPTY and numOccupied == 0:
         newMap[i] += OCCUPIED
-      elif char == OCCUPIED and numOccupied >= 4:
+      elif char == OCCUPIED and numOccupied >= maxOccupied:
         newMap[i] += EMPTY
       else:
         newMap[i] += char
-  
   return newMap
 
 
-def answer1(input):
+def answer1(input, getAdjFn = getAdj1, maxOccupied = 4):
   old = input.split('\n')
-  new = applyRules(old)
+  new = applyRules(old, getAdjFn, maxOccupied)
   while old != new:
     old = new.copy()
-    new = applyRules(old)
-  
+    new = applyRules(old, getAdjFn, maxOccupied)
   return len([x for x in ''.join(new) if x == OCCUPIED])
 
 
+def answer2(input):
+  return answer1(input, getAdj2, 5)
+
 
 f = open('input.txt', 'r')
-print(answer1(f.read()))
+print(answer2(f.read()))
  
