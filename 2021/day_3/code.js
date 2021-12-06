6532a4a462a695dc71fb45e570679ae7269655fe
Molly Henderson <molly.henderson@oliveai.com>
2021-12-06 14:43:10 -0500

    Wrap up day 3

--- 2021/day_3/code.js
+++ 2021/day_3/code.js
@@ -1,20 +1,49 @@
 const fs = require('fs');
 
-const answer1 = (input) => {
+const getOnesCounts = (input) =>
+  input[0].split('')
+    .map((_, i) =>
+      input.reduce((acc, d) =>
+        acc + parseInt(d[i]), 0));
+
+const getGamma = (input) => {
   const total = input.length;
-  const onesCounts = input[0].split('').map((_, i) => {
-    return input.reduce((acc, d) => {
-      return acc + parseInt(d[i]);
-    }, 0);
-  });
+  return getOnesCounts(input)
+    .map(count =>
+      +(count >= total / 2))
+    .join('');
+}
 
-  const gamma = parseInt(onesCounts.map(count => {
-    return +(count > total / 2);
-  }).join(''), 2);
+const getEpsilon = (input) => {
+  const total = input.length;
+  return getOnesCounts(input)
+    .map(count =>
+      +(count < total / 2))
+    .join('');
+}
 
-  const epsilon = parseInt(onesCounts.map(count => {
-    return +(count <= total / 2);
-  }).join(''), 2);
+const filterList = (input, compareFn) => {
+  let list = [...input];
+  let i = 0;
+  let compareList = compareFn(list);
+  while (list.length > 1) {
+    list = list.filter(x => x[i] === compareList[i]);
+    i += 1;
+    compareList = compareFn(list);
+  }
+  return list[0];
+}
+
+const answer2 = (input) => {
+  const oxygenRating = parseInt(filterList(input, getGamma), 2);
+  const co2Rating = parseInt(filterList(input, getEpsilon), 2);
+
+  return { oxygenRating, co2Rating, answer: oxygenRating * co2Rating };
+}
+
+const answer1 = (input) => {
+  const gamma = parseInt(getGamma(input), 2);
+  const epsilon = parseInt(getEpsilon(input), 2);
 
   return { gamma, epsilon, answer: gamma * epsilon };
 }
@@ -23,4 +52,4 @@ const FILENAME = 'input.txt';
 
 const f = fs.readFileSync(FILENAME, 'utf-8');
 const input = f.split('\n');
-console.log(answer1(input));
+console.log(answer2(input));
