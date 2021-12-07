94158ee90bb0f518ae2bc08d8f8af235d7771356
Molly Henderson <molly.henderson@oliveai.com>
2021-12-07 10:21:54 -0500

    Part 2

--- 2021/day_7/code.js
+++ 2021/day_7/code.js
@@ -1,5 +1,24 @@
 const fs = require('fs');
 
+const answer2 = (input) => {
+  const positions = input.sort((x, y) => x - y);
+
+  let cheapest = null;
+  for (let i = positions[0]; i < positions[positions.length-1]; i++) {
+    // simulate this move
+    const cost = positions.reduce((acc, val) => {
+      const delta = Math.abs(val - i);
+      // (n(n + 1)) / 2, where n = delta - 1
+      return acc + ((delta ** 2) - ((delta * (delta - 1)) / 2));
+    }, 0);
+
+    if (!cheapest || cost < cheapest) {
+      cheapest = cost;
+    }
+  }
+  return cheapest;
+}
+
 const answer1 = (input) => {
   const positions = input.sort((x, y) => x - y);
 
@@ -22,4 +41,4 @@ const FILENAME = 'input.txt';
 
 const f = fs.readFileSync(FILENAME, 'utf-8');
 const input = f.split(',');
-console.log(answer1(input));
+console.log(answer2(input));
