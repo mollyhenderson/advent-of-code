8258b47d19be4d63bbe861ad92a90c8167bbdf2f
Molly Henderson <molly.henderson@oliveai.com>
2021-12-10 10:40:45 -0500

    Part 2

--- 2021/day_10/code.js
+++ 2021/day_10/code.js
@@ -10,6 +10,33 @@ const scoreMap = {
   '>': 25137,
 }
 
+const answer2 = (input) => {
+  const scores = [];
+  input.forEach(line => {
+    let score = 0;
+    let q = [];
+    for (const c of line.split('')) {
+      if (openingChars.includes(c)) {
+        q.push(c);
+      } else {
+        const lastOpening = q.pop();
+        if (openingChars.indexOf(lastOpening) !== closingChars.indexOf(c)) {
+          // corrupt line; skip
+          return;
+        }
+      }
+    }
+    // update score for each leftover char
+    q.reverse().forEach(c => {
+      score *= 5;
+      score += openingChars.indexOf(c) + 1;
+    });
+    scores.push(score);
+  });
+  scores.sort((a, b) => a - b);
+  return scores[Math.floor(scores.length / 2)];
+}
+
 const answer1 = (input) => {
   let score = 0;
   const q = [];
@@ -33,4 +60,4 @@ const FILENAME = 'input.txt';
 
 const f = fs.readFileSync(FILENAME, 'utf-8');
 const input = f.split('\n');
-console.log(answer1(input));
+console.log(answer2(input));
