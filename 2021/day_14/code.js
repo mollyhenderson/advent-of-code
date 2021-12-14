ad248b449055ce42d621b7dd0032679b8d4fcc26
Molly Henderson <henderson.molly4@gmail.com>
2021-12-14 01:43:59 -0500

    Add some very important commentary (get it comment-ary)

--- 2021/day_14/code.js
+++ 2021/day_14/code.js
@@ -23,6 +23,41 @@ const parseInput = (input) => {
   }
 }
 
+const answer1 = (input) => {
+  const STEPS = 10;
+
+  let { template, rules } = parseInput(input);
+
+  for (let i = 0; i < STEPS; i++) {
+    const chars = template.split('');
+    template = chars[0];
+    for (let j = 0; j < chars.length-1; j++) {
+      const pair = chars.slice(j, j+2);
+      const rule = rules.find(r => r.matches(pair));
+      if (rule) template += rule.element;
+      template += chars[j+1];
+    }
+  }
+  
+  const allElements = new Set(template);
+  const countMap = template.split('').reduce((acc, val) => {
+    if (!acc[val]) acc[val] = 0;
+    acc[val]++;
+    return acc;
+  }, {});
+  const counts = Object.values(countMap);
+  const max = Math.max(...counts);
+  const min = Math.min(...counts);
+  return max - min;
+}
+
+/*
+ * I would like it to be known that everything below this point can be used to solve both parts of today's puzzle.
+ * Don't you love it when this silly challenge basically forces you to solve a problem twice?
+ * (it was actually very fun but let me act bitter anyway)
+ * Thank you for attending my Ted talk
+ */
+
 const makeMatrix = (size) => {
   const matrix = [];
   for (let i = 0; i < size; i++) {
@@ -74,34 +109,6 @@ const answer2 = (input) => {
 
   const max = Math.max(...finalCounts);
   const min = Math.min(...finalCounts);
-  return max - min;
-}
-
-const answer1 = (input) => {
-  const STEPS = 10;
-
-  let { template, rules } = parseInput(input);
-
-  for (let i = 0; i < STEPS; i++) {
-    const chars = template.split('');
-    template = chars[0];
-    for (let j = 0; j < chars.length-1; j++) {
-      const pair = chars.slice(j, j+2);
-      const rule = rules.find(r => r.matches(pair));
-      if (rule) template += rule.element;
-      template += chars[j+1];
-    }
-  }
-  
-  const allElements = new Set(template);
-  const countMap = template.split('').reduce((acc, val) => {
-    if (!acc[val]) acc[val] = 0;
-    acc[val]++;
-    return acc;
-  }, {});
-  const counts = Object.values(countMap);
-  const max = Math.max(...counts);
-  const min = Math.min(...counts);
   return max - min;
 }
 
