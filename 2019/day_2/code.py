e48eb08792906314019e78224614c1550a2e618b
Molly Henderson <henderson.molly4@gmail.com>
2021-12-16 23:26:26 -0500

    2019, day 2, part 2

--- 2019/day_2/code.py
+++ 2019/day_2/code.py
@@ -1,15 +1,25 @@
+def answer2(program):
+  for noun in range(100):
+    for verb in range(100):
+      input = [x for x in program]
+      input[1] = noun
+      input[2] = verb
+      out = answer1(input)
+      if (out == 19690720):
+        return 100 * noun + verb
+
 def answer1(program):
   program = [int(x) for x in program]
-  program[1] = 12
-  program[2] = 2
+  # program[1] = 12
+  # program[2] = 2
   i = 0
   while True:
-    opcode, in1, in2, out, *_ = program[i:]
+    opcode, param1, param2, param3, *_ = program[i:]
     match opcode:
         case 1:
-          program[out] = program[in1] + program[in2]
+          program[param3] = program[param1] + program[param2]
         case 2:
-          program[out] = program[in1] * program[in2]
+          program[param3] = program[param1] * program[param2]
         case 99:
           return program[0]
         case _:
@@ -20,4 +30,4 @@ FILENAME = 'input.txt'
 
 f = open(FILENAME, 'r')
 input = f.read().split(',')
-print(answer1(input))
+print(answer2(input))
