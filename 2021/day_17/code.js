c617fbf478072639e610ed562dc7765404120d4f
Molly Henderson <henderson.molly4@gmail.com>
2021-12-17 00:50:46 -0500

    Improve the render function

--- 2021/day_17/code.js
+++ 2021/day_17/code.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 
-const render = (x, y, target) => {
+const render = (steps, target) => {
   const { minX, maxX, minY, maxY } = target;
-  for (let i = Math.max(0, y); i >= minY; i--) {
+
+  const overallMaxY = Math.max(...steps.map(s => s.y));
+  for (let i = overallMaxY; i >= minY; i--) {
     let line = '';
     for (let j = 0; j <= maxX; j++) {
       if (i === 0 && j === 0) line += 'S';
-      else if (i === y && j === x) line += '#';
+      else if (steps.some(s => s.x === j && s.y === i)) line += '#';
       else if (i <= maxY && j >= minX) line += 'T';
       else line += '.';
     }
@@ -19,14 +21,14 @@ const launch = (xVelocity, yVelocity, target) => {
   const { minX, maxX, minY, maxY } = target;
   const steps = [];
   while (x < maxX && y > maxY) {
+    steps.push({x, y});
     x += xVelocity;
     y += yVelocity;
     if (xVelocity > 0) xVelocity -= 1;
     else if (xVelocity < 0) xVelocity += 1;
     yVelocity -= 1;
-    // render(x, y, target);
-    steps.push({x, y});
   }
+  render(steps, target);
   if (x >= minX && x <= maxX && y >= minY && y <= maxY) return Math.max(...steps.map(s => s.y));
   return false;
 }
@@ -39,9 +41,11 @@ const parseInput = (input) => {
 const answer1 = (input) => {
   const target = parseInput(input);
   // TODO: find x,y such that maxY is maximized
+  // binary search that mofo?
   let x = 7;
   let y = 2;
   const maxY = launch(x, y, target);
+  return maxY;
 }
 
 const FILENAME = 'test_input.txt';
