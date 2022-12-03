705ee1c16bb5b8b5f885d2255b2f19b34d0785be
Molly Henderson <henderson.molly4@gmail.com>
2022-12-02 23:37:34 -0500

    Add python support to the boilerplate generator

--- newDay.js
+++ newDay.js
@@ -1,8 +1,13 @@
 const fs = require('fs')
 const path = require('path')
-const { program } = require('commander')
+const { program, Option } = require('commander')
 
-const initialContents = `const utils = require('../../utils/utils')
+const Languages = {
+  JAVASCRIPT: 'javascript',
+  PYTHON: 'python'
+}
+
+const jsContents = `const utils = require('../../utils/utils')
 
 module.exports.answer2 = (input) => {
   return 'This function is not yet implemented!'
@@ -13,10 +18,25 @@ module.exports.answer1 = (input) => {
 }
 `
 
+// TODO: add python support to index.js & clean up this template
+const pyContents = `def answer2(lines):
+  return 'This function is not yet implemented!'
+
+def answer1(lines):
+  return 'This function is not yet implemented!'
+
+FILENAME = 'test_input.txt'
+
+f = open(FILENAME, 'r')
+input = f.read().split('\\n')
+print(answer1(input))
+`
+
 program
   .argument('<year>')
   .argument('<day>')
-  .action((year, day) => {
+  .addOption(new Option('-l, --language <language>', 'Which language to generate.').choices(Object.values(Languages)).default(Languages.JAVASCRIPT))
+  .action((year, day, { language }) => {
     const directory = path.join(process.cwd(), year, `day_${day}`)
 
     try {
@@ -26,11 +46,18 @@ program
       return
     }
 
-    const codeFile = path.join(directory, 'code.js')
+    if (language === Languages.PYTHON) {
+      const codeFile = path.join(directory, 'code.py')
+      fs.writeFileSync(codeFile, pyContents)
+    } else {
+      const codeFile = path.join(directory, 'code.js')
+      fs.writeFileSync(codeFile, jsContents)
+    }
+
+    
     const testInputFile = path.join(directory, 'test_input.txt')
     const inputFile = path.join(directory, 'input.txt')
 
-    fs.writeFileSync(codeFile, initialContents)
     fs.writeFileSync(testInputFile, '')
     fs.writeFileSync(inputFile, '')
   })
