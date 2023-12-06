b7bca5055975e2bcbe949edc504249c5698f0cf3
Molly Henderson <molly.henderson@bubble.io>
2023-12-05 21:07:38 -0500

    Part twoooooo

--- 2023/04/code.js
+++ 2023/04/code.js
@@ -1,7 +1,28 @@
 const { getLines, int } = require('../../utils/helpers')
 
+const countWins = (line) => {
+  const [winningSide, mySide] = line.replace(/Card\s+\d+:/, '').split('|')
+
+  // Fun fact, had several issues here with my regex (whaaaat? nooooo...)
+  // Honestly I was gonna write them out as a lil retro but I don't remember
+  // them all and I've lost steam
+  const winningNumbers = winningSide.match(/(\d+)\s*/g).map(int)
+  const myNumbers = mySide.match(/(\d+)\s*/g).map(int)
+
+  return myNumbers.filter((n) => winningNumbers.includes(n)).length
+}
+
 module.exports.answer2 = (input) => {
-  
+  const lines = getLines(input)
+  const winArr = lines.map(countWins)
+
+  const winData = winArr.map((w) => ({ wins: w, count: 1 }))
+  winData.forEach(({ wins, count }, i) => {
+    for (let j = i; j < i + wins; j++) {
+      winData[j + 1].count = winData[j + 1].count + count
+    }
+  })
+  return winData.reduce((sum, { count }) => sum + count, 0)
 }
 
 module.exports.answer1 = (input) => {
@@ -9,18 +30,9 @@ module.exports.answer1 = (input) => {
 
   let total = 0
   lines.map((l) => {
-    const [winningSide, mySide] = l.replace(/Card\s+\d+:/, '').split('|')
-
-    // Fun fact, had several issues here with my regex (whaaaat? nooooo...)
-    // Honestly I was gonna write them out as a lil retro but I don't remember
-    // them all and I've lost steam
-    const winningNumbers = winningSide.match(/(\d+)\s*/g).map(int)
-    const myNumbers = mySide.match(/(\d+)\s*/g).map(int)
-
-    const wins = myNumbers.filter((n) => winningNumbers.includes(n))
-
-    if (wins.length) {
-      const points = 2 ** (wins.length - 1)
+    const wins = countWins(l)
+    if (wins) {
+      const points = 2 ** (wins - 1)
       total += points
     }
   })
