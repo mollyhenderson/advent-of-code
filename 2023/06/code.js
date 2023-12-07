5ed11f72371913c928fb13d00cfbd511d8372cfd
Molly Henderson <molly.henderson@bubble.io>
2023-12-07 00:02:58 -0500

    Weeeee

--- 2023/06/code.js
+++ 2023/06/code.js
@@ -1,4 +1,4 @@
-const { getLines, parseNumbers } = require('../../utils/helpers')
+const { getLines, parseNumbers, int } = require('../../utils/helpers')
 
 const parseInput = (input) => {
   const lines = getLines(input)
@@ -7,26 +7,31 @@ const parseInput = (input) => {
   return times.map((t, i) => ({ time: t, distance: distances[i] }))
 }
 
-const getWinWays = (races) => {
-  return races.map(({ time, distance }) => {
-    let i = 0
-    while (i < time) {
-      const thisDist = i * (time - i)
-      if (thisDist > distance) {
-        // we won!
-        return time - (2 * i - 1)
-      }
-      i++
+const parseInput2 = (input) => {
+  const lines = getLines(input)
+  const time = lines[0].replace(/Time:|\s/g, '')
+  const distance = lines[1].replace(/Distance:|\s/g, '')
+  return { time: int(time), distance: int(distance) }
+}
+
+const getWinWay = ({ time, distance }) => {
+  let i = 0
+  while (i < time) {
+    const thisDist = i * (time - i)
+    if (thisDist > distance) {
+      // we won!
+      return time - (2 * i - 1)
     }
-  })
+    i++
+  }
 }
 
 module.exports.answer2 = (input) => {
-  return 'This function is not yet implemented!'
+  return getWinWay(parseInput2(input))
 }
 
 module.exports.answer1 = (input) => {
   const races = parseInput(input)
-  const winWays = getWinWays(races)
+  const winWays = races.map(getWinWay)
   return winWays.reduce((product, curr) => product * curr, 1)
 }
