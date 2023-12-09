b48f297cb2789493b04d69ab799ea7480ef669ae
Molly Henderson <henderson.molly4@gmail.com>
2023-12-09 16:24:08 -0500

    Day 9, part 1 - I almost fell trap to the classic "my helper function doesn't support negative numbers correctly" blunder

---
+++ 2023/09/code.js
@@ -0,0 +1,49 @@
+const { getLines, parseNumbers, sum } = require('../../utils/helpers')
+
+class Sequence {
+  constructor(line) {
+    this.numbers = parseNumbers(line)
+  }
+
+  predictNext() {
+    let next = [...this.numbers]
+    this.nexts = [next]
+
+    while (next.some((n) => n !== 0)) {
+      next = getNextSequence(next)
+      this.nexts.push(next)
+    }
+
+    this.nexts.at(-1).push(0)
+
+    for (let i = this.nexts.length - 2; i >= 0; i--) {
+      const prev = this.nexts[i + 1]
+      const curr = this.nexts[i]
+      curr.push(prev.at(-1) + curr.at(-1))
+    }
+
+    return this.nexts[0].at(-1)
+  }
+}
+
+const parseInput = (input) => {
+  return getLines(input).map((l) => new Sequence(l))
+}
+
+const getNextSequence = (seq) => {
+  const nextSeq = []
+  for (let i = 1; i < seq.length; i++) {
+    nextSeq.push(seq[i] - seq[i - 1])
+  }
+  return nextSeq
+}
+
+module.exports.answer2 = (input) => {
+  return 'This function is not yet implemented!'
+}
+
+module.exports.answer1 = (input) => {
+  const sequences = parseInput(input)
+  const nexts = sequences.map((s) => s.predictNext())
+  return sum(nexts)
+}
