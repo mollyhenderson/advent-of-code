39d2d5f815820be36b6be1caaac52e3dece748e7
Molly Henderson <molly.henderson@percipiohealth.com>
2024-12-07 10:24:00 -0500

    The easiest part 2

--- 2024/07/code.js
+++ 2024/07/code.js
@@ -15,7 +15,26 @@ const parseInput = (input) => {
 }
 
 module.exports.answer2 = (input) => {
-  return 'This function is not yet implemented!'
+  const equations = parseInput(input)
+
+  const valids = equations.filter((equation) => {
+    const { testNum, nums } = equation
+
+    const possibilities = [nums.shift()]
+    for (const num of nums) {
+      const len = possibilities.length
+      for (let j = 0; j < len; j++) {
+        possibilities.push(possibilities[j] * num)
+        // This is the only difference :shrug:
+        possibilities.push(parseInt(`${possibilities[j]}${num}`))
+        possibilities[j] += num
+      }
+    }
+
+    if (possibilities.includes(testNum)) return true
+  })
+
+  return valids.reduce((sum, e) => (sum += e.testNum), 0)
 }
 
 module.exports.answer1 = (input) => {
