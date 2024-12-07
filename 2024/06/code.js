752bea3b7c8e0e525e45c0f33e9cec52490e7c17
Molly Henderson <molly.henderson@percipiohealth.com>
2024-12-07 09:24:11 -0500

    Wrap up day 6

--- 2024/06/code.js
+++ 2024/06/code.js
@@ -3,6 +3,8 @@ const { getLines, getCharacters } = require('../../utils/helpers')
 class Spot {
   visited = false
 
+  hits = []
+
   constructor(char, x, y) {
     this.char = char
     this.x = x
@@ -21,6 +23,19 @@ class Spot {
     this.visited = true
   }
 
+  hitFrom(direction) {
+    this.hits.push(direction)
+  }
+
+  hasBeenHitFrom(direction) {
+    return this.hits.includes(direction)
+  }
+
+  reset() {
+    this.visited = false
+    this.hits = []
+  }
+
   toString() {
     return this.visited ? '!' : this.char
   }
@@ -31,45 +46,65 @@ const move = ({ direction, spot, lab }) => {
     spot.visit()
     switch (direction) {
       case 'UP': {
-        if (spot.y === 0) return
+        if (spot.y === 0) return true
 
         const next = lab.get(spot.y - 1, spot.x)
         if (next.isFree()) {
           spot = next
         } else {
+          if (next.hasBeenHitFrom(direction)) {
+            // guard's in an infinite loop
+            return false
+          }
+          next.hitFrom(direction)
           direction = 'RIGHT'
         }
         break
       }
       case 'DOWN': {
-        if (spot.y === lab.matrix.length - 1) return
+        if (spot.y === lab.matrix.length - 1) return true
 
         const next = lab.get(spot.y + 1, spot.x)
         if (next.isFree()) {
           spot = next
         } else {
+          if (next.hasBeenHitFrom(direction)) {
+            // guard's in an infinite loop
+            return false
+          }
+          next.hitFrom(direction)
           direction = 'LEFT'
         }
         break
       }
       case 'LEFT': {
-        if (spot.x === 0) return
+        if (spot.x === 0) return true
 
         const next = lab.get(spot.y, spot.x - 1)
         if (next.isFree()) {
           spot = next
         } else {
+          if (next.hasBeenHitFrom(direction)) {
+            // guard's in an infinite loop
+            return false
+          }
+          next.hitFrom(direction)
           direction = 'UP'
         }
         break
       }
       case 'RIGHT': {
-        if (spot.x === lab.matrix[0].length - 1) return
+        if (spot.x === lab.matrix[0].length - 1) return true
 
         const next = lab.get(spot.y, spot.x + 1)
         if (next.isFree()) {
           spot = next
         } else {
+          if (next.hasBeenHitFrom(direction)) {
+            // guard's in an infinite loop
+            return false
+          }
+          next.hitFrom(direction)
           direction = 'DOWN'
         }
         break
@@ -91,11 +126,36 @@ class Lab {
     )
   }
 
+  reset() {
+    for (let row of this.matrix) {
+      for (let spot of row) {
+        spot.reset()
+      }
+    }
+  }
+
   traverse() {
     const spot = this.guard
     let direction = 'UP'
 
-    move({ direction, spot, lab: this })
+    return move({ direction, spot, lab: this })
+  }
+
+  findObstaclePositions() {
+    let count = 0
+    for (let i = 0; i < this.matrix.length; i++) {
+      for (let j = 0; j < this.matrix[0].length; j++) {
+        const spot = this.get(i, j)
+        if (spot.isFree()) {
+          spot.char = '#'
+          const res = this.traverse()
+          if (!res) count++
+          spot.char = '.'
+          this.reset()
+        }
+      }
+    }
+    return count
   }
 
   countVisited() {
@@ -118,7 +178,13 @@ class Lab {
 }
 
 module.exports.answer2 = (input) => {
-  return 'This function is not yet implemented!'
+  // The idea:
+  // for each obstacle spot, record a list of "hit from" directions
+  // if hitting from the same direction multiple times, we've hit a loop.
+  // Then we can try placing a new obstacle at each spot
+  // and test whether traverse hits a loop
+  const lab = new Lab(input)
+  return lab.findObstaclePositions()
 }
 
 module.exports.answer1 = (input) => {
