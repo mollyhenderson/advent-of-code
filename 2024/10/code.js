fd85d8db1055f0e7e1d3228393add41b68c9f49d
Molly Henderson <molly.henderson@percipiohealth.com>
2024-12-10 10:09:37 -0500

    Part 2!

--- 2024/10/code.js
+++ 2024/10/code.js
@@ -19,7 +19,40 @@ class TopoMap extends Map {
 }
 
 module.exports.answer2 = (input) => {
-  return 'This function is not yet implemented!'
+  const map = new TopoMap(input)
+
+  const trailheads = map.getTrailheads()
+
+  let totalScore = 0
+  for (const trailhead of trailheads) {
+    // search
+    const nodesToCheck = [trailhead]
+    while (nodesToCheck.length) {
+      const node = nodesToCheck.pop()
+      if (node.char === '9') {
+        // we did it
+        // go team
+        const trail = [node]
+        let traverser = node
+        while (traverser.prev) {
+          trail.push(traverser.prev)
+          traverser = traverser.prev
+        }
+        // Store a list of trails per trailhead
+        traverser.addTrail(trail)
+      }
+      const nextNeighbors = map
+        .neighbors(node.x, node.y)
+        .filter((n) => parseInt(n.char) === parseInt(node.char) + 1)
+      nextNeighbors.forEach((n) => (n.prev = node))
+      nodesToCheck.push(...nextNeighbors)
+    }
+
+    /// This is the only difference!
+    const score = trailhead.trails.length
+    totalScore += score
+  }
+  return totalScore
 }
 
 module.exports.answer1 = (input) => {
