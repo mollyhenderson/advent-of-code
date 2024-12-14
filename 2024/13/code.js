4712925db7b2edbb6b0714b30098ba80ca18c636
Molly Henderson <molly.henderson@percipiohealth.com>
2024-12-13 20:24:12 -0500

    Today's part 1!

---
+++ 2024/13/code.js
@@ -0,0 +1,70 @@
+const { getLines, int, sum } = require('../../utils/helpers')
+
+class Button {
+  constructor(line, cost) {
+    const {
+      groups: { x, y },
+    } = line.match(/X\+(?<x>\d+), Y\+(?<y>\d+)/)
+    this.x = int(x)
+    this.y = int(y)
+    this.cost = cost
+  }
+
+  toString() {
+    return `(${this.x}, ${this.y}) for ${this.cost} credits`
+  }
+}
+
+class ClawMachine {
+  constructor(input) {
+    this.buttonA = new Button(input[0], 3)
+    this.buttonB = new Button(input[1], 1)
+
+    const {
+      groups: { x, y },
+    } = input[2].match(/X=(?<x>\d+), Y=(?<y>\d+)/)
+    this.x = int(x)
+    this.y = int(y)
+  }
+}
+
+const parseInput = (input) => {
+  const lines = getLines(input)
+  const machines = []
+  for (let i = 0; i < lines.length; i += 4) {
+    machines.push(new ClawMachine(lines.slice(i, i + 3)))
+  }
+  return machines
+}
+
+module.exports.answer2 = (input) => {
+  return 'This function is not yet implemented!'
+}
+
+module.exports.answer1 = (input) => {
+  const machines = parseInput(input)
+
+  let sum = 0
+  for (const machine of machines) {
+    const options = []
+    for (let aPresses = 0; aPresses <= 100; aPresses++) {
+      const x = machine.buttonA.x * aPresses
+      const y = machine.buttonA.y * aPresses
+      const remainderX = machine.x - x
+      const remainderY = machine.y - y
+
+      const bPresses = remainderX / machine.buttonB.x
+
+      if (
+        Number.isInteger(bPresses) &&
+        remainderY / machine.buttonB.y === bPresses
+      ) {
+        options.push({ a: aPresses, b: bPresses })
+      }
+    }
+    if (options.length) {
+      sum += options[0].a * 3 + options[0].b
+    }
+  }
+  return sum
+}
