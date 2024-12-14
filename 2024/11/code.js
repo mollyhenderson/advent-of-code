89ba866204e37619221e1edb44f612a9d3908c20
Molly Henderson <molly.henderson@percipiohealth.com>
2024-12-14 15:02:39 -0500

    Finally got day 11!

--- 2024/11/code.js
+++ 2024/11/code.js
@@ -16,8 +16,34 @@ const blink = (nums) => {
   return res
 }
 
+const memo = {}
+const blinkTimes = (num, count) => {
+  const key = `${num}/${count}`
+  if (memo[key]) return memo[key]
+  if (count === 0) return 1
+
+  const str = num.toString()
+  if (num === 0) {
+    res = blinkTimes(1, count - 1)
+  } else if (str.length % 2 === 0) {
+    const left = str.slice(0, str.length / 2)
+    const right = str.slice(str.length / 2)
+    res = blinkTimes(int(left), count - 1) + blinkTimes(int(right), count - 1)
+  } else {
+    res = blinkTimes(num * 2024, count - 1)
+  }
+  memo[key] = res
+  return res
+}
+
 module.exports.answer2 = (input) => {
-  return 'This function is not yet implemented!'
+  const BLINKS = 75
+  let nums = parseNumbers(input)
+  let sum = 0
+  for (const num of nums) {
+    sum += blinkTimes(num, BLINKS)
+  }
+  return sum
 }
 
 module.exports.answer1 = (input) => {
