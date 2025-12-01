8754429dea42b02e880004abd8262b93dac3d94a
Molly Henderson <molly.henderson@percipiohealth.com>
2025-12-01 00:41:47 -0500

    Part 2

--- 2025/01/code.js
+++ 2025/01/code.js
@@ -1,7 +1,38 @@
 const { getLines } = require('../../utils/helpers')
 
+const parseLine = (line) => {
+  const [, dir, n] = line.match(/(L|R)(\d+)/)
+  const amount = +n
+
+  return { dir, amount }
+}
+
 module.exports.answer2 = (input) => {
-  return 'This function is not yet implemented!'
+  let num = 50
+  let count = 0
+
+  const lines = getLines(input)
+  for (const line of lines) {
+    const { dir, amount } = parseLine(line)
+
+    if (dir === 'R') {
+      num += amount
+    } else {
+      num -= amount
+    }
+
+    while (num < 0 || num > 99) {
+      if (num > 99) {
+        num = num - 100
+        count++
+      }
+      if (num < 0) {
+        num = num + 100
+        count++
+      }
+    }
+  }
+  return count
 }
 
 module.exports.answer1 = (input) => {
@@ -10,8 +41,7 @@ module.exports.answer1 = (input) => {
 
   const lines = getLines(input)
   for (const line of lines) {
-    const [, dir, n] = line.match(/(L|R)(\d+)/)
-    const amount = +n
+    const { dir, amount } = parseLine(line)
 
     if (dir === 'R') {
       num += amount
@@ -24,7 +54,7 @@ module.exports.answer1 = (input) => {
         num = num - 100
       }
       if (num < 0) {
-        num = 100 - Math.abs(num)
+        num = num + 100
       }
     }
 
