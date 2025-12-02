d71a8f5585c8562ecc2dd99cd547680cfd5f6daf
Molly Henderson <molly.henderson@percipiohealth.com>
2025-12-02 09:20:53 -0500

    Day 2, part 1

---
+++ 2025/02/code.js
@@ -0,0 +1,29 @@
+const { getLines } = require('../../utils/helpers')
+
+const parseInput = (input) => {
+  const ranges = input.split(',')
+  return ranges.map((range) => {
+    const [start, end] = range.split('-')
+    return [+start, +end]
+  })
+}
+
+module.exports.answer2 = (input) => {
+  return 'This function is not yet implemented!'
+}
+
+module.exports.answer1 = (input) => {
+  const ranges = parseInput(input)
+
+  const badIds = []
+  for (const [start, end] of ranges) {
+    for (let i = start; i <= end; i++) {
+      const match = `${i}`.match(/^(\d+)(\1)$/)
+      if (match) {
+        badIds.push(i)
+      }
+    }
+  }
+
+  return badIds.reduce((sum, curr) => (sum += curr), 0)
+}
