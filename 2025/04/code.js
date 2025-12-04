6ff60a9ca4e6a2d044285d828cf1e84c656262b6
Molly Henderson <molly.henderson@percipiohealth.com>
2025-12-04 00:19:27 -0500

    Refaccy

--- 2025/04/code.js
+++ 2025/04/code.js
@@ -1,5 +1,25 @@
 const { Map } = require('../../utils/helpers')
 
+const getNeighbors = (map, x, y) => {
+  const neighbors = []
+  if (map.isInBounds(x - 1, y)) neighbors.push(map.at(x - 1, y))
+  if (map.isInBounds(x + 1, y)) neighbors.push(map.at(x + 1, y))
+  if (map.isInBounds(x, y - 1)) neighbors.push(map.at(x, y - 1))
+  if (map.isInBounds(x, y + 1)) neighbors.push(map.at(x, y + 1))
+
+  if (map.isInBounds(x - 1, y - 1)) neighbors.push(map.at(x - 1, y - 1))
+  if (map.isInBounds(x - 1, y + 1)) neighbors.push(map.at(x - 1, y + 1))
+  if (map.isInBounds(x + 1, y - 1)) neighbors.push(map.at(x + 1, y - 1))
+  if (map.isInBounds(x + 1, y + 1)) neighbors.push(map.at(x + 1, y + 1))
+
+  return neighbors
+}
+
+const isAccessible = (map, x, y) => {
+  const neighbors = getNeighbors(map, x, y)
+  return neighbors.filter((n) => n.char === '@').length < 4
+}
+
 module.exports.answer2 = (input) => {
   return 'This function is not yet implemented!'
 }
@@ -13,19 +33,7 @@ module.exports.answer1 = (input) => {
     for (let x = 0; x < map.width(); x++) {
       const node = map.at(x, y)
       if (node.char !== '@') continue
-
-      const neighbors = []
-      if (map.isInBounds(x - 1, y)) neighbors.push(map.at(x - 1, y))
-      if (map.isInBounds(x + 1, y)) neighbors.push(map.at(x + 1, y))
-      if (map.isInBounds(x, y - 1)) neighbors.push(map.at(x, y - 1))
-      if (map.isInBounds(x, y + 1)) neighbors.push(map.at(x, y + 1))
-
-      if (map.isInBounds(x - 1, y - 1)) neighbors.push(map.at(x - 1, y - 1))
-      if (map.isInBounds(x - 1, y + 1)) neighbors.push(map.at(x - 1, y + 1))
-      if (map.isInBounds(x + 1, y - 1)) neighbors.push(map.at(x + 1, y - 1))
-      if (map.isInBounds(x + 1, y + 1)) neighbors.push(map.at(x + 1, y + 1))
-
-      if (neighbors.filter((n) => n.char === '@').length < 4) {
+      if (isAccessible(map, x, y)) {
         accessible.push(node)
       }
     }
