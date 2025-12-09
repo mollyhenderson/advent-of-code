655cef4a742b30e2e4cae7d0a7316b4acae546c2
Molly Henderson <molly.henderson@percipiohealth.com>
2025-12-09 18:33:53 -0500

    Finally wrapped up day 5

--- 2025/05/code.js
+++ 2025/05/code.js
@@ -2,14 +2,36 @@ const { getLines } = require('../../utils/helpers')
 const _ = require('lodash')
 
 module.exports.answer2 = (input) => {
-  return 'This function is not yet implemented!'
+  const [r] = input.split('\n\n')
+
+  const ranges = getLines(r)
+    .map((rangeString) => rangeString.split('-').map((n) => +n))
+    .sort(([a], [b]) => a - b)
+
+  let count = 0
+
+  let lastEnd = 0
+  for (const range of ranges) {
+    let [start, end] = range
+    if (end <= lastEnd) {
+      // Complete overlap; we can essentially ignore this range
+      continue
+    }
+    if (start <= lastEnd) {
+      // There is overlap; don't over-count
+      start = lastEnd + 1
+    }
+    count += end - start + 1 // The addt'l 1 is bc the range is inclusive
+    lastEnd = end
+  }
+  return count
 }
 
 module.exports.answer1 = (input) => {
   const [r, a] = input.split('\n\n')
 
   const ranges = getLines(r)
-  let available = getLines(a).map(n => +n)
+  let available = getLines(a).map((n) => +n)
   const freshIngredients = []
 
   for (const range of ranges) {
